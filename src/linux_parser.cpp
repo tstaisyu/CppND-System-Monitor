609854deb89fe5a292ca671e5f39bbb27dcf6943
taisyu shibata <t_shiba117@yahoo.co.jp>
2023-07-04 22:13:08 +0900

    Initial commit

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -68,32 +68,123 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() { 
+  string line;
+  string Title, Value;
+  string Total, Free;
+  std::ifstream stream(kProcDirectory + kMeminfoFilename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> Title >> Value) {
+        if (Title == "MemTotal") {
+          Total = Value;
+          break;
+        }
+      }
+      while (linestream >> Title >> Value) {
+        if (Title == "MemFree") {
+          Free = Value;
+          break;
+        }
+      }
+    }
+  }
+  return stoi(Free) / stoi(Total);
+}
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  string Value;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> Value;
+  }
+  return stol(Value);
+}
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies() { 
+  return LinuxParser::UpTime() * sysconf(_SC_CLK_TCK);
+}
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
 long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
 
 // TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
+long LinuxParser::ActiveJiffies() { 
+  long value;
+  for (auto v : LinuxParser::CpuUtilization() ) {
+    value += stol(v);
+  }
+  return value;
+}
 
 // TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
+long LinuxParser::IdleJiffies() { 
+  return stol(LinuxParser::CpuUtilization()[3]) + stol(LinuxParser::CpuUtilization()[4]);
+}
 
 // TODO: Read and return CPU utilization
-vector<string> LinuxParser::CpuUtilization() { return {}; }
+vector<string> LinuxParser::CpuUtilization() { 
+  string line;
+  string Title, v0, v1 ,v2, v3, v4, v5, v6, v7, v8, v9;
+  string Total, Free;
+  std::ifstream stream(kProcDirectory + kMeminfoFilename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> Title >> v0 >> v1 >> v2 >> v3 >> v4 >> v5 >> v6 >> v7 >> v8 >> v9) {
+        if (Title == "cpu") {
+          break;
+        }
+      }
+    }
+  }
+  return {v0, v1 ,v2, v3, v4, v5, v6, v7, v8, v9};
+}  
+
 
 // TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses() { 
+  string line;
+  string Title, Value;
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> Title >> Value) {
+        if (Title == "processes") {
+          break;
+        }
+      }
+    }
+  }
+  return stoi(Value);
+}
 
 // TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+int LinuxParser::RunningProcesses() { 
+  string line;
+  string Title, Value;
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  if (stream.is_open()) {
+    while (std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> Title >> Value) {
+        if (Title == "procs_running") {
+          break;
+        }
+      }
+    }
+  }
+  return stoi(Value);
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
